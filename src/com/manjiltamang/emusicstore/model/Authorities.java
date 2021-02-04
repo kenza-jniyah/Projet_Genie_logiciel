e61463ad6db86c65e330d0c656c8d4f6189b532d
kenza-jniyah <65117871+kenza-jniyah@users.noreply.github.com>
2021-02-04 13:38:54 +0100

    Update Authorities.java

--- src/com/manjiltamang/emusicstore/model/Authorities.java
+++ src/com/manjiltamang/emusicstore/model/Authorities.java
@@ -19,7 +19,7 @@ public class Authorities {
 		this.authoritiesId = authoritiesId;
 	}
 	public String getUsername() {
-		return username
+		return username;
 	}
 	public void setUsername(String username) {
 		this.username = username;
